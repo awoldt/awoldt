1c3cf422fca751c5cc64f4d4ab136d339e7a33c1
unknown <abreckwoldt23@gmail.com>
2022-09-15 15:01:03 -0700

    stable

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -151,40 +151,7 @@ export default function Home() {
             </a>
           </Button>
 
-          <Button
-            style={{
-              display: "block",
-              borderBottomRightRadius: "10px",
-              borderBottomLeftRadius: "10px",
-              borderTopLeftRadius: "0px",
-              borderTopRightRadius: "0px",
-              marginTop: "5px",
-              width: '115.52px'
-            }}
-            variant={"light"}
-            className="mx-auto"
-          >
-            <a
-              href={"https://medium.com/@awoldt"}
-              target={"_blank"}
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                style={{ marginRight: "5px" }}
-                fill="currentColor"
-                className="bi bi-medium"
-                viewBox="0 0 16 16"
-              >
-                <path d="M9.025 8c0 2.485-2.02 4.5-4.513 4.5A4.506 4.506 0 0 1 0 8c0-2.486 2.02-4.5 4.512-4.5A4.506 4.506 0 0 1 9.025 8zm4.95 0c0 2.34-1.01 4.236-2.256 4.236-1.246 0-2.256-1.897-2.256-4.236 0-2.34 1.01-4.236 2.256-4.236 1.246 0 2.256 1.897 2.256 4.236zM16 8c0 2.096-.355 3.795-.794 3.795-.438 0-.793-1.7-.793-3.795 0-2.096.355-3.795.794-3.795.438 0 .793 1.699.793 3.795z" />
-              </svg>
-              <span>Medium</span>
-            </a>
-          </Button>
-
+       
           <Link
             to="/projects"
             title="View all projects"
@@ -210,6 +177,9 @@ export default function Home() {
           </Link>
         </motion.div>
         <motion.div animate={{ y: [-1000, 0], opacity: [0, 1] }}>
+
+
+            
           <Row style={{ marginTop: "100px" }} className="text-center">
             <Col lg={5} style={{ padding: "0px" }}>
               <p style={{ fontSize: ptagSize }}>
