5e4580856dda2bcab0e8e02d48ad666a944f35c3
awoldt <abreckwoldt23@gmail.com>
2023-10-23 16:21:26 -0400

    stable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import GoogleAnalytics from "@/components/GoogleAnalytics";
+import AnalyticsScript from "@/components/AnalyticsScript";
 import "../style/globals.css";
 
 export default function RootLayout({
@@ -21,7 +21,7 @@ export default function RootLayout({
           integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
           crossOrigin="anonymous"
         ></script>
-        <GoogleAnalytics />
+        <AnalyticsScript />
       </head>
       <body>{children}</body>
     </html>
