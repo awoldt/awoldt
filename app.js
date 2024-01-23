467cffae2f55ca3a8c38718015a73683192b488c
awoldt <abreckwoldt23@gmail.com>
2024-01-23 10:18:28 -0500

    articles now use ejs templates instead of html

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import path from "path";
+import fs from "fs/promises";
 
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
 app.use(express.static(path.join(process.cwd(), "public")));
 
 app.get("/", (req, res) => {
@@ -13,22 +16,47 @@ app.get("/projects", (req, res) => {
 app.get("/articles", (req, res) => {
   res.sendFile(path.join(process.cwd(), "pages", "articles.html"));
 });
-
-app.get(`/articles/:NAME`, (req, res) => {
-  res.sendFile(
-    path.join(process.cwd(), "pages", "articles", `${req.params.NAME}.html`)
-  );
+app.get(`/articles/:NAME`, async (req, res) => {
+  const relatedArticles = await GetRelatedArticles(req.params.NAME);
+  res.render(`articles/${req.params.NAME}`, {
+    readMoreLinks: relatedArticles,
+  });
 });
 
-// PERMENENT REDIRECT
+// --- PERMENENT REDIRECT FROM OLD BLOGS ROUTE (NOW CALLED ARTICLES)
 app.get("/blogs", (req, res) => {
   res.redirect(301, `/articles`);
 });
 app.get("/blogs/:ANYHTING", (req, res) => {
   res.redirect(301, `/articles/${req.params.ANYHTING}`);
 });
-//
+// ---
 
 app.listen(8080, () => {
   console.log("\nserver listening on port 8080");
 });
+
+async function GetRelatedArticles(currentPath) {
+  /* 
+    Will return other articles for reader OTHER than
+    the current articles being viewed
+
+    Used to show read more links at bottom of each article
+  */
+
+  try {
+    const articles = await fs.readFile(
+      path.join(process.cwd(), "articles_data.json"),
+      { encoding: "utf-8" }
+    );
+
+    const articlesData = (await JSON.parse(articles)).filter((x) => {
+      return x.path !== currentPath;
+    });
+
+    return articlesData;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
