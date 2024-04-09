d0d0c4dbd7f9bdda7668cf509678fa14c3e8aead
awoldt <abreckwoldt23@gmail.com>
2024-04-09 00:01:27 -0400

    stable

--- views/ArticlePage.tsx
+++ views/ArticlePage.tsx
@@ -2,13 +2,6 @@ import Nav from "./components/Nav";
 import type { ArticleData } from "../utils/functions";
 import MoreArticlesLinks from "./components/MoreArticlesLinks";
 
-/* 
-    The html string being passed into dangerouslySetInnerHTML is coming 
-    from this server and does not contain any dangerous html code.... 
-
-    no need to be scared :)
-*/
-
 interface Data {
   content: string;
   head: string;
